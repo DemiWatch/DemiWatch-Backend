1cd2a9e39f52fb47cb4509d41efe2ce2b3a0b927
AdiSyahrul <adisyahrul27@gmail.com>
2023-09-30 15:19:59 +0700

    add status

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ const register = async (req, res) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
   if (!emailRegex.test(email)) {
-      return res.status(400).json({ error: 'Invalid email format' });
+      return res.status(400).json({ status: 400, error: 'Invalid email format' });
   }
 
   try {
@@ -20,9 +20,9 @@ const register = async (req, res) => {
 
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = await User.create({ name, email, password: hashedPassword });
-      return res.status(200).json({ success: true, message: 'User registered successfully' });
+      return res.status(200).json({ status: 200, success: true, message: 'User registered successfully' });
   } catch (error) {
-      return res.status(500).json({ error: `Failed to register user: ${error.message}` });
+      return res.status(500).json({ status: 500, error: `Failed to register user: ${error.message}` });
   }
 };
 
@@ -33,7 +33,7 @@ const login = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(401).json({ error: 'Invalid credentials', detail: 'Email not found' });
+      return res.status(401).json({ status: 401, error: 'Invalid credentials', detail: 'Email not found' });
     }
 
     const match = await bcrypt.compare(password, user.password);
@@ -42,12 +42,12 @@ const login = async (req, res) => {
       const token = jwt.sign({ email: user.email, name: user.name }, jwtSecret, {
         expiresIn: '1h'
       });
-      return res.status(200).json({ success: true, name: user.name, token: token });
+      return res.status(200).json({ status: 200, success: true, name: user.name, token: token });
     } else {
-      return res.status(401).json({ error: 'Invalid credentials', detail: 'Incorrect password' });
+      return res.status(401).json({ status: 401, error: 'Invalid credentials', detail: 'Incorrect password' });
     }
   } catch (error) {
-    return res.status(500).json({ error: `Failed to log in: ${error.message}` });
+    return res.status(500).json({ status: 500, error: `Failed to log in: ${error.message}` });
   }
 };
 
