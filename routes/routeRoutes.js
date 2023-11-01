0f5a3e53d40fdd81953c673b5bd9c07d42d90618
AdiSyahrul <adisyahrul27@gmail.com>
2023-11-01 15:04:29 +0700

    header auth livelocation

--- routes/routeRoutes.js
+++ routes/routeRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const routeController = require('../controllers/routeController');
+const authenticate = require('../middleware/authMiddleware');
 
 router.get('/getRoute', routeController.getRoute);
-router.post('/liveLocation', routeController.liveLocation);
+router.post('/liveLocation',authenticate, routeController.liveLocation);
 router.get('/getLocation', routeController.getLocation);
 
 module.exports = router;
